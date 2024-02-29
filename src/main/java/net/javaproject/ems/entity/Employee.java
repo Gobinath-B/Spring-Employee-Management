e15e9011a74883149d9ced9fa8c04be2dd65930d
Gobinath B <princegopinath42@gmail.com>
2024-02-29 12:49:47 +0530

    feat employee entity

--- src/main/java/net/javaproject/ems/entity/Employee.java
+++ src/main/java/net/javaproject/ems/entity/Employee.java
@@ -1,4 +1,30 @@
 package net.javaproject.ems.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.annotation.processing.Generated;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstname;
+    @Column(name = "last_name")
+    private String lastname;
+
+    @Column(name = "email",nullable = false,unique = true)
+    private String email;
+
 }
