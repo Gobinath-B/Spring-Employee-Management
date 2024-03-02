cc49c99b8844e6ea9b301403ab2f861963bcd0ec
Gobinath B <princegopinath42@gmail.com>
2024-03-02 23:18:52 +0530

    feat controller code for get employee by id method

--- src/main/java/net/javaproject/ems/controller/EmpoyeeController.java
+++ src/main/java/net/javaproject/ems/controller/EmpoyeeController.java
@@ -5,10 +5,7 @@ import net.javaproject.ems.dto.EmployeeDto;
 import net.javaproject.ems.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -17,10 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class EmpoyeeController {
      private EmployeeService employeeService;
+
+     //POST request
      @PostMapping
 
      public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
          EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
          return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
      }
+
+     //GET REQUEST
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeebyId(@PathVariable("id") Long employeeId){
+         EmployeeDto employeeDto = employeeService.getEmployeebyId(employeeId);
+         return ResponseEntity.ok(employeeDto);
+    }
 }
